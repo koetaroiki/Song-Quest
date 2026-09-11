a52e67aae422305a1f4b6de42e0d3f1cdd76fc38
koetaroiki <koetaroiki@outlook.com>
2026-09-11 11:13:20 +0900

    headerとfooterをスクロール時に隠す

--- assets/scripts/scroll.js
+++ assets/scripts/scroll.js
@@ -1,18 +1,21 @@
 const main = document.querySelector(".lyt-main");
 const header = document.querySelector(".lyt-header");
 const footer = document.querySelector(".lyt-footer");
+const container = document.querySelector(".lyt-global_container");
 
 let scrollTimer;
 
 main.addEventListener("scroll", () => {
   header.classList.add("is-hidden");
   footer.classList.add("is-hidden");
+  container.classList.add("is-scroll");
 
   clearTimeout(scrollTimer);
 
   scrollTimer = setTimeout(() => {
     header.classList.remove("is-hidden");
     footer.classList.remove("is-hidden");
+    container.classList.remove("is-scroll");
   }, 1000);
 });
 
@@ -22,12 +25,14 @@ panels.forEach(panel => {
   panel.addEventListener("scroll", () => {
     header.classList.add("is-hidden");
     footer.classList.add("is-hidden");
+    container.classList.add("is-scroll");
 
     clearTimeout(scrollTimer);
 
     scrollTimer = setTimeout(() => {
       header.classList.remove("is-hidden");
       footer.classList.remove("is-hidden");
+      container.classList.remove("is-scroll");
     }, 1000);
   });
 });
